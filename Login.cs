127a1b49fc5d9ab06a5f40ff88eb76c5b48cfae5
AriHarry <34508094+AriHarry@users.noreply.github.com>
2018-05-15 12:56:01 +0530

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginInMVC4WithEF.Models
+{
+    public class Login
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string passwordsalt { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Boolean IsActive { get; set; }
+    }
+}
